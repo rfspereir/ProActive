40518b639d0e073a8179ca258028fd71452f4794
Rafael Pereira <contato@rafael-pereira.com>
2024-03-24 22:07:42 -0300

    Firebase Frontend include
    Adicionado a biblioteca do firebase ao projeto (ng add @angular/fire)

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# GeladeiraInteligente
+# proactive
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
 
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"proactive-ae334","appId":"1:98316423271:web:594349f7713fdbd0f8c4c2","databaseURL":"https://proactive-ae334-default-rtdb.firebaseio.com","storageBucket":"proactive-ae334.appspot.com","apiKey":"AIzaSyDuCIrTT_CQjwBTzwdqT8exzWlqmqrs2ao","authDomain":"proactive-ae334.firebaseapp.com","messagingSenderId":"98316423271"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
