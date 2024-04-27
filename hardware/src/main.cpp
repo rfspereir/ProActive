89f8dda56a90773d157d3f17f336fd1e521d31a4
Rafael Pereira <contato@rafael-pereira.com>
2024-04-26 22:49:30 -0300

    Firebase.h
    Separação das funções de conexão em um arquivo de biblioteca.

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,10 +1,9 @@
-#include <WiFi.h>
+
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 #include <ESP32Servo.h>
-#include <Firebase_ESP_Client.h>
-#include "addons/TokenHelper.h" //Provide the token generation process info.
-#include "addons/RTDBHelper.h"//Provide the RTDB payload printing info and other helper functions.
+
+#include "firebase.h"
 
 #define WIFI_SSID "TI-01 0380"
 #define WIFI_PASSWORD "b#0642R2"
@@ -21,15 +20,6 @@ String posPath= BASE_DIR + String("pos");
 #define API_KEY "AIzaSyDuCIrTT_CQjwBTzwdqT8exzWlqmqrs2ao"
 //RTDB URLefine the RTDB URL */
 #define DATABASE_URL "https://proactive-ae334-default-rtdb.firebaseio.com/" 
-
-//Define Firebase Data object
-FirebaseData fbdo;
-
-FirebaseAuth auth;
-FirebaseConfig config;
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
 
 const int DOOR_SENSOR_PIN = 23;
 const int BUZZER_PIN = 19;
@@ -48,169 +38,12 @@ int doorState;
 unsigned long doorOpenTime = 0;
 unsigned long currentTime = 0;
 
-void initWiFi(void)
-{
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println("------Conexao WI-FI------");
-  Serial.print("Conectando-se na rede: ");
-  Serial.println(WIFI_SSID);
-  Serial.println("Aguarde");
-    while (WiFi.status() != WL_CONNECTED){
-      Serial.print(".");
-      delay(300);
-
-  Serial.println();
-  Serial.print("Conectado com o IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-  }
-}
-
-void conectarFirebase(void)
-{
-  config.api_key = API_KEY;
-  config.database_url = DATABASE_URL;
-  if (Firebase.signUp(&config, &auth, "", ""))
-  {
-    Serial.println("Conectado ao Firebase");
-    signupOK = true;
-  }
-  else
-  {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-
-    /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-}
-
-void sendDataToFirebaseFloat(float value, String path)
-{
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
-  {
-    sendDataPrevMillis = millis();
-    if (Firebase.RTDB.setFloat(&fbdo, path, value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-}
-
-void sendDataToFirebaseInt(int value, String path)
-{
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
-  {
-    sendDataPrevMillis = millis();
-    if (Firebase.RTDB.setInt(&fbdo, path, value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-}
-
-void sendDataToFirebaseString(String value, String path)
-{
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
-  {
-    sendDataPrevMillis = millis();
-    if (Firebase.RTDB.setString(&fbdo, path, value))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-}
-
-
-int getDataFromFirebaseInt(String path)
-{
-  if (Firebase.ready() && signupOK)
-  {
-    if (Firebase.RTDB.getInt(&fbdo, path))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-      return fbdo.intData();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-  return -1;
-}
-
-float getDataFromFirebaseFloat(String path)
-{
-  if (Firebase.ready() && signupOK)
-  {
-    if (Firebase.RTDB.getFloat(&fbdo, path))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-      return fbdo.floatData();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-  return -1;
-}
-
-String getDataFromFirebaseString(String path)
-{
-  if (Firebase.ready() && signupOK)
-  {
-    if (Firebase.RTDB.getString(&fbdo, path))
-    {
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-      return fbdo.stringData();
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-  return "";
-}
-
 void setup()
 {
   Serial.begin(115200);
 
-  initWiFi();
-  conectarFirebase();
+  initWiFi(WIFI_SSID, WIFI_PASSWORD);
+  conectarFirebase(API_KEY, DATABASE_URL);
   
   pinMode(DOOR_SENSOR_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
